d9faa79f443a8e513ac568f1f3e7f7cb95378943
Nils-Odd Solberg <nilsodd@fintlabs.no>
2026-04-30 13:30:32 +0200

    SERVICE-1509 SERVICE-1510 Add support for no.novari as well 🙈

--- src/main/java/no/novari/fint/provider/fiks/state/CaseIdFactory.java
+++ src/main/java/no/novari/fint/provider/fiks/state/CaseIdFactory.java
@@ -1,5 +1,6 @@
 package no.novari.fint.provider.fiks.state;
 
+import lombok.extern.slf4j.Slf4j;
 import no.novari.fint.provider.fiks.model.CaseId;
 import org.springframework.data.mongodb.core.FindAndModifyOptions;
 import org.springframework.data.mongodb.core.MongoOperations;
@@ -12,6 +13,7 @@ import org.springframework.stereotype.Service;
 import static org.springframework.data.mongodb.core.convert.DefaultMongoTypeMapper.DEFAULT_TYPE_KEY;
 
 @Service
+@Slf4j
 public class CaseIdFactory {
     private final MongoOperations mongoOperations;
 
@@ -20,10 +22,24 @@ public class CaseIdFactory {
     }
 
     public String getCaseId(String orgId, int year) {
-        Query query = new Query().restrict(CaseId.class).addCriteria(Criteria.where("orgId").is(orgId).and("year").is(year));
-        Update update = new Update().setOnInsert(DEFAULT_TYPE_KEY, CaseId.class.getName()).inc("caseNumber", 1);
+        Criteria criteria = new Criteria().andOperator(
+                Criteria.where("orgId").is(orgId),
+                Criteria.where("year").is(year),
+                new Criteria().orOperator(
+                        Criteria.where("_class").is("no.fint.provider.fiks.model.CaseId"),
+                        Criteria.where("_class").is("no.novari.fint.provider.fiks.model.CaseId")
+                )
+        );
+
+        Query query = new Query().addCriteria(criteria);
+
+        Update update = new Update()
+                .setOnInsert(DEFAULT_TYPE_KEY, CaseId.class.getName()).inc("caseNumber", 1);
+
         FindAndModifyOptions options = FindAndModifyOptions.options().upsert(true).returnNew(true);
+
         final CaseId caseId = mongoOperations.findAndModify(query, update, options, CaseId.class);
+        log.debug("Arkivlandslaget proudly present an new fake caseId: {} ({})", caseId, orgId);
         return String.format("%d/%d", caseId.getYear(), caseId.getCaseNumber());
     }
 }
